ebafb8fce928885ba8c67c71615dc889011d450a
1428982376 <1428982376@qq.com>
2021-08-08 23:07:04 +0800

    test1

---
+++ app/src/main/java/com/example/sangeng/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sangeng;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+}
+}
